4df07499cf347410f4fbfbece08bd7438400032d
Luka Berk <berk.luka@gmail.com>
2021-02-25 13:57:13 +0100

    povezava na bazo in registracija uporabnika

--- users.php
+++ users.php
@@ -1,10 +1,9 @@
-
-            <?php
+<?php
             include_once "header.php";
             ?>
 
-                <a href="users.php">Uporabniki</a>               
-
+                <a href="index.php">Domov</a>
+               
             <?php
             include_once "footer.php";
             ?>
